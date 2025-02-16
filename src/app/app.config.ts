357b02032febcd176a1f43f0e56458a997668608
luecha.kan <luecha.kanm@gmail.com>
2025-02-16 13:03:37 +0700

    first add project angular19 tutorial

---
+++ src/app/app.config.ts
@@ -0,0 +1,34 @@
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { withInMemoryScrolling } from '@angular/router';
+import { routes } from './app.routes';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideHttpClient(withFetch()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      })
+    ),
+    provideClientHydration(withEventReplay()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
+};
