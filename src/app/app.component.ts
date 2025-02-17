abcd724491f129f6fc3d5fff5c6740f61a95b4a4
luecha.kan <luecha.kanm@gmail.com>
2025-02-17 20:42:15 +0700

    Install Tailwind CSS and add first page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdminComponent } from './features/admin/admin.component';
+import { UserComponent } from './features/user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AdminComponent, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Angular19-Tutorial';
